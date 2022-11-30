69e988ba90294f2593ce4253e5f875308280b353
Leonel Herrera <leoherre@usc.edu>
2022-11-30 22:10:04 +0000

    Initial commit for hw5

---
+++ wordle.cpp
@@ -0,0 +1,34 @@
+// For debugging
+#include <iostream>
+// For std::remove
+#include <algorithm> 
+#include <map>
+#include <set>
+#include "wordle.h"
+#include "dict-eng.h"
+using namespace std;
+
+
+// Add prototypes of helper functions here
+
+
+// Definition of primary wordle function
+std::set<std::string> wordle(
+    const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict)
+{
+    // Add your code here
+
+}
+//he user will provide you an input of how many letters the word is
+//what letters have already been guessed in their correct location
+//what letters have already been guessed in their correct location
+
+// Define any helper functions here
+//wordlehelper(word, ){
+  //for a thru z{
+    //check if valid word, 
+    
+  }
+}
