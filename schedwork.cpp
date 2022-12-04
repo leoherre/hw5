02edd4104c4bae618ed8c5cc01cff8f452b86191
Leonel Herrera <leoherre@usc.edu>
2022-12-04 06:43:27 +0000

    worlde and schedwork for first time, lol

--- schedwork.cpp
+++ schedwork.cpp
@@ -19,8 +19,18 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+//copies of schedule() by reference 
+void scheduleHelper(
+  const vector<vector<bool>>& cpyofAvail,
+  vector<vector<Worker_T>>& cpyofsched,
+  int workerAtten [],
+  int& day,
+  const size_t dailyNeed,
+  const size_t maxShifts,
+  bool& valSched
+);
 
-
+bool notWorking(const Worker_T&worker, vector<vector<Worker_T>>& cpyofsched, const int& day);
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
@@ -35,9 +45,95 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+  //insert workers into avail matrix
+  vector<Worker_T> temp;
+  for(int i = 0; i < (int)avail.size();i++){
+    sched.push_back(temp);
+  }
 
-
-
-
+  int size = avail[0].size();
+  //initialize workersAtten [] to 0
+  int workerAtten[size];
+  for(int i = 0; i < size;i++){
+    workerAtten[i] = 0;
+  }
+   
+  bool validSched = false;
+  int day = 0;
+  scheduleHelper(avail, sched, workerAtten, day,dailyNeed,maxShifts, validSched);
+  return validSched;
 }
+
+bool notWorking(const Worker_T& worker, vector<vector<Worker_T>> & schedule, const int& day){
+  //0 so worker is not scheduled 
+  if(schedule[day].size() == 0){
+    return true;
+  }
+  else{
+    //loop through workers 
+    for(int i = 0; i < (int)schedule[day].size(); i++){
+      //worker is aready booked so return false 
+      if(schedule[day][i] == worker){
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+void scheduleHelper(
+  const vector<vector<bool>>& cpyofAvail,
+  vector<vector<Worker_T>>& cpyofsched,
+  int workerAtten [],
+  int& day,
+  const size_t dailyNeed,
+  const size_t maxShifts,
+  bool& valSched 
+)
+{
+  
+  for(int i = 0;i < (int) cpyofAvail[0].size();i++){
+    Worker_T worker = (unsigned int) i;
+    //worker is free, maxshifts not reached, worker is not working 
+    if(cpyofAvail[day][i] == true && workerAtten[i] < (int) maxShifts &&
+     notWorking(worker, cpyofsched, day)){
+       workerAtten[i] +=1;
+       cpyofsched[day].push_back(worker);
+      //enough workers are reached 
+       if(cpyofsched[day].size() == dailyNeed){
+         //day/schedule is still valid 
+         if(day < (int)cpyofAvail.size()-1){
+           day +=1;
+           scheduleHelper(cpyofAvail, cpyofsched, workerAtten, day, dailyNeed, maxShifts, valSched);
+           if(valSched == true){
+             return;
+           }
+
+           else{
+             workerAtten[i] -= 1;
+             day -= 1;
+             cpyofsched[day].pop_back();
+           }
+         }
+         //not valid schedule 
+         else{
+           valSched = true;
+         }
+       }
+       //not enough workers, so recurse 
+         else if(cpyofsched[day].size() != dailyNeed){
+           scheduleHelper(cpyofAvail, cpyofsched, workerAtten, day, dailyNeed, maxShifts, valSched);
+           if(valSched != false){
+             return;
+           }
+           else{
+             workerAtten[i] -= 1;
+             cpyofsched[day].pop_back();
+           }
+         }
+     }
+  }
+}
+
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -10,6 +10,14 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+//by reference 
+std::set<std::string> wordleHelper(
+  std::set<std::string>& usefulWords,
+  std::string& possibleWords,
+  std::string& floatLetters,
+  const std::set<std::string>& dict,
+  int pos,
+  int spaces);
 
 
 // Definition of primary wordle function
@@ -19,16 +27,78 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string> usefulWords;
+    std::string possibleWords = in;
+    std:: string floatYellow = floating;
+    int spaces = 0;
 
+    //get the # of spaces 
+    for(unsigned int i = 0; i < (unsigned int)in.size(); i++){
+      if(in[i] == '-'){
+        spaces++;
+      }
+    }
+    //recurse and return usefulWords set
+    wordleHelper(usefulWords, possibleWords, floatYellow, dict, 0, spaces);
+    return usefulWords;
 }
-//he user will provide you an input of how many letters the word is
-//what letters have already been guessed in their correct location
-//what letters have already been guessed in their correct location
 
-// Define any helper functions here
-//wordlehelper(word, ){
-  //for a thru z{
-    //check if valid word, 
-    
+std::set<std::string> wordleHelper(
+  std::set<std::string>& usefulWords,
+  std::string& possibleWords,
+  std::string& floatLetters,
+  const std::set<std::string>& dict,
+  int pos,
+  int spaces)
+  {
+    //base case,  all spaces are full and possibly word is valid 
+    if(pos == (int)possibleWords.size()){
+      //word is valid 
+      if(dict.find(possibleWords) != dict.end()){
+        usefulWords.insert(possibleWords);
+      }
+      return usefulWords;
+    }
+    //backtracking implementation
+    if(possibleWords[pos] != '-'){
+      wordleHelper(usefulWords, possibleWords, floatLetters, dict, pos+1, spaces);
+      return usefulWords;
+    }
+    //floating letter is present 
+    if(spaces <= (int)floatLetters.size()){
+      //iterate through floating letters and check with that particualr letter 
+      for(unsigned int i = 0; i < (unsigned int)floatLetters.size();i++){
+        std::string temp = floatLetters;
+        possibleWords[pos] = floatLetters[i];
+        floatLetters.erase(floatLetters.find(floatLetters[i]), 1);
+        wordleHelper(usefulWords, possibleWords, floatLetters, dict, pos, spaces-1);
+          floatLetters = temp;
+      }
+      //reverse back to '-' after done 
+      possibleWords[pos] = '-';
+    }
+    else if(spaces > (int)floatLetters.size()){
+      //Go through entire alphabet and recurse 
+      for(char letter = 'a';letter <= 'z';letter++){
+        possibleWords[pos] = letter;
+        //letter is in the floating letters 
+        if(floatLetters.find(letter) != floatLetters.npos){
+          std::string temp = floatLetters;
+          floatLetters.erase(floatLetters.find(letter),1);
+          wordleHelper(usefulWords,possibleWords,floatLetters, dict, pos, spaces-1);
+          floatLetters = temp;
+        }
+        //keep recursing since letter is not in floating letters
+        else{
+          wordleHelper(usefulWords, possibleWords, floatLetters, dict, pos, spaces-1);
+        }
+      }
+      //reverse back to '-' after done
+      possibleWords[pos] = '-';
+    }
+    return usefulWords;
   }
-}
+//the user will provide you an input of how many letters the word is
+//what letters have already been guessed in their correct location
+//what letters have already been guessed in their correct location
+
